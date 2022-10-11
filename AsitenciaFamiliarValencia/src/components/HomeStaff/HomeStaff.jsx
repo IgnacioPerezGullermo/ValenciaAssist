9dbe9a5026fb93a842dfffea4fa2df0b56e95d89
El Bebelo <nacho71197@gmail.com>
2022-10-11 18:44:17 -0300

    Home terminado

--- AsitenciaFamiliarValencia/src/components/HomeStaff/HomeStaff.jsx
+++ AsitenciaFamiliarValencia/src/components/HomeStaff/HomeStaff.jsx
@@ -3,142 +3,42 @@ import './HomeStaff.scss';
 import Silvia from '../../assets/silvia-bgless.png';
 import Caty from '../../assets/cate-bgless.png';
 import { useSpring, animated } from 'react-spring';
+import { StaffCard } from './StaffCard';
+
+const info = {
+  silvia: {
+    name: 'silvia',
+    bio: 'Soy Silvia Yaques, Licenciada en Trabajo Social (UBA) y trabajé 27 años en la obra social más grande de Argentina (PAMI); participé en la creación de programas de promoción de salud, fui Jefa de Agencia en el Servicio de Trabajo Social, coordiné encuentros con entidades de salud mental reconocidas y coordiné servicios en centros de día, hogares y residencias geriátricas.',
+    desc: 'Licenciada en Trabajo Social',
+  },
+  caty: {
+    name: 'caterina',
+    bio: 'Soy Caterina Del Carlo, Licenciada en Terapia Ocupacional (UAI) y trabajé en fundación INECO ad honórem para formarme y luego como T.O. de planta, para prestadoras de salud como Swiss Medical, servicio privado y atención en consultorios particulares individuales y grupales. Además, participé de charlas y congresos a la comunidad, Día del Alzheimer, Evaluaciones Funcionales y tratamientos a pacientes en el Departamento de Neurociencias de la Fundación Favaloro (BA, Arg.).',
+    desc: 'Licenciada en Terapia Ocupacional',
+  },
+};
 
 export const HomeStaff = () => {
   const [Focus, setFocus] = useState('');
 
-  const props1 = useSpring({
-    to: { opacity: 1 },
-    from: { opacity: 0 },
-    config: { duration: 800 },
-  });
-  const props2 = useSpring({
-    to: { width: '100vw' },
-    from: { width: '50vw' },
-    config: { duration: 1500 },
-  });
-  const props2re = useSpring({
-    to: { width: '50vw' },
-    from: { width: '100vw' },
-    config: { duration: 1500 },
-    delay: 500,
-    immediate: true,
-  });
-  const props3 = useSpring({
-    to: { width: '40vw', marginLeft: '20vw', top: '30%', left: '0%' },
-    from: { width: '25vw', marginLeft: '0vw' },
-    config: { duration: 1500 },
-  });
-  const props3re = useSpring({
-    to: { width: '25vw', marginLeft: '0vw' },
-    from: { width: '40vw', marginLeft: '20vw', top: '40%' },
-    config: { duration: 1500 },
-    immediate: true,
-  });
-  const props5 = useSpring({
-    to: { width: '40vw', marginRight: '10vw', top: '30%' },
-    from: { width: '29vw', marginRight: '0vw' },
-    config: { duration: 1500 },
-  });
-  const props5re = useSpring({
-    to: { width: '29vw', marginRight: '0vw' },
-    from: { width: '40vw', marginRight: '10vw', top: '40%' },
-    immediate: true,
-  });
-
-  const props4 = useSpring({
-    to: { scale: 1.2 },
-    from: { scale: 1 },
-    config: { duration: 1000 },
-  });
-  const props6re = useSpring({
-    to: { opacity: 1 },
-    from: { opacity: 0, zIndex: 2 },
-    delay: 1000,
-  });
-  const props6 = useSpring({
-    to: { opacity: 0, zIndex: 2 },
-    from: { opacity: 1 },
-  });
-
-  const handleClick = (info) => {
-    if (Focus === 'silvia' || Focus === 'caty') {
-      setFocus('');
-    } else {
-      setFocus(info);
-    }
-  };
-
   return (
-    <animated.div style={props1} className="HomeStaff-container">
+    <div className="HomeStaff-container">
       {/* <Spring> */}
-      {Focus === 'caty' ? (
-        <animated.div
-          style={Focus === 'caty' ? props6 : props6re}
-          className="HomeStaff-container-leftSide"
-        >
-          <animated.img
-            style={Focus === 'silvia' ? props4 : null}
-            className="HomeStaff-container-leftSide-Silvia"
-            src={Silvia}
-            alt="silvia"
-          />
-          <animated.div
-            style={Focus === 'silvia' ? props3 : null}
-            className="HomeStaff-container-leftSide-TextBox"
-          >
-            <h1>Silvia</h1>
-            <h4>Licenciada en Trabajo Social</h4>
-            {Focus === 'silvia' ? (
-              <p>
-                Soy Silvia Yaques, Licenciada en Trabajo Social (UBA) y trabajé
-                27 años en la obra social más grande de Argentina (PAMI);
-                participé en la creación de programas de promoción de salud, fui
-                Jefa de Agencia en el Servicio de Trabajo Social, coordiné
-                encuentros con entidades de salud mental reconocidas y coordiné
-                servicios en centros de día, hogares y residencias geriátricas.
-              </p>
-            ) : null}
-            <button onClick={() => handleClick('silvia')}>
-              {Focus === 'silvia' ? 'Volver' : 'Leer mas'}
-            </button>
-          </animated.div>
-        </animated.div>
-      ) : (
-        <animated.div
-          style={Focus === 'silvia' ? props2 : props2re}
-          className="HomeStaff-container-leftSide"
-        >
-          <animated.img
-            style={Focus === 'silvia' ? props4 : null}
-            className="HomeStaff-container-leftSide-Silvia"
-            src={Silvia}
-            alt="silvia"
-          />
-          <animated.div
-            style={Focus === 'silvia' ? props3 : props3re}
-            className="HomeStaff-container-leftSide-TextBox"
-          >
-            <h1>Silvia</h1>
-            <h4>Licenciada en Trabajo Social</h4>
-            {Focus === 'silvia' ? (
-              <p>
-                Soy Silvia Yaques, Licenciada en Trabajo Social (UBA) y trabajé
-                27 años en la obra social más grande de Argentina (PAMI);
-                participé en la creación de programas de promoción de salud, fui
-                Jefa de Agencia en el Servicio de Trabajo Social, coordiné
-                encuentros con entidades de salud mental reconocidas y coordiné
-                servicios en centros de día, hogares y residencias geriátricas.
-              </p>
-            ) : null}
-            <button onClick={() => handleClick('silvia')}>
-              {Focus === 'silvia' ? 'Volver' : 'Leer mas'}
-            </button>
-          </animated.div>
-        </animated.div>
-      )}
-
-      {Focus === 'silvia' ? (
+      <StaffCard
+        setFocus={setFocus}
+        Focus={Focus}
+        data={info.silvia}
+        side="left"
+        pic={Silvia}
+      ></StaffCard>
+      <StaffCard
+        setFocus={setFocus}
+        Focus={Focus}
+        data={info.caty}
+        side="right"
+        pic={Caty}
+      ></StaffCard>
+      {/* {Focus === 'silvia' ? (
         <animated.div
           style={Focus === 'silvia' ? props6 : props6re}
           className="HomeStaff-container-rightSide"
@@ -158,7 +58,7 @@ export const HomeStaff = () => {
         </animated.div>
       ) : (
         <animated.div
-          style={Focus === 'caty' ? props2 : props2re}
+          style={Focus === 'caty' ? props2 : props6re}
           className="HomeStaff-container-rightSide"
         >
           <animated.img
@@ -191,7 +91,7 @@ export const HomeStaff = () => {
             </button>
           </animated.div>
         </animated.div>
-      )}
-    </animated.div>
+      )} */}
+    </div>
   );
 };
---
+++ AsitenciaFamiliarValencia/src/components/HomeStaff/StaffCard.jsx
@@ -0,0 +1,129 @@
+import React, { useState } from 'react';
+import './StaffCard.scss';
+import Silvia from '../../assets/silvia-bgless.png';
+import Caty from '../../assets/cate-bgless.png';
+import { useSpring, animated } from 'react-spring';
+
+export const StaffCard = ({ setFocus, Focus, data, side, pic }) => {
+  const handleClick = (info) => {
+    if (Focus === 'silvia' || Focus === 'caterina') {
+      setFocus('');
+    } else {
+      setFocus(info);
+    }
+  };
+  const props6 = useSpring({
+    to: {
+      width: '40vw',
+      marginRight: '10vw',
+      top: '30%',
+      left: '60%',
+      textAling: 'left',
+    },
+    from: { width: '29vw', marginRight: '0vw' },
+  });
+  const props5 = useSpring({
+    to: {
+      width: '40vw',
+      top: '30%',
+      right: '30%',
+      textAlign: 'right',
+    },
+    from: { width: '29vw', right: '-10%' },
+  });
+  const props4 = useSpring({
+    to: { scale: 1.2, left: '15%' },
+    from: { scale: 1 },
+  });
+  const props4alt = useSpring({
+    to: { scale: 1.2, left: '70%' },
+    from: { scale: 1 },
+  });
+  const props3 = useSpring({
+    to: {
+      width: '30vw',
+      top: '30%',
+      left: '45%',
+      opacity: 1,
+      textAlign: 'left',
+    },
+    from: { width: '25vw', marginLeft: '0vw', opacity: 0 },
+  });
+  const props2 = useSpring({
+    to: { width: '100vw' },
+    from: { width: '50vw' },
+  });
+  const props1 = useSpring({
+    to: { width: '50vw' },
+    from: { width: '100vw' },
+  });
+  if (Focus === data.name || Focus === '') {
+    return (
+      <animated.div
+        style={Focus === data.name ? props2 : null}
+        className={
+          side === 'left'
+            ? 'HomeStaff-container-leftSide'
+            : 'HomeStaff-container-rightSide'
+        }
+      >
+        {Focus === 'silvia' ? (
+          <animated.img
+            style={Focus === 'silvia' ? props4 : null}
+            className={
+              side === 'left'
+                ? 'HomeStaff-container-leftSide-Silvia'
+                : 'HomeStaff-container-rightSide-Caty'
+            }
+            src={pic}
+            alt="silvia"
+          />
+        ) : (
+          <animated.img
+            style={Focus === 'caterina' ? props4alt : null}
+            className={
+              side === 'left'
+                ? 'HomeStaff-container-leftSide-Silvia'
+                : 'HomeStaff-container-rightSide-Caty'
+            }
+            src={pic}
+            alt="silvia"
+          />
+        )}
+        {Focus === 'silvia' ? (
+          <animated.div
+            style={Focus === 'silvia' ? props3 : null}
+            className={
+              side === 'left'
+                ? 'HomeStaff-container-leftSide-TextBox'
+                : 'HomeStaff-container-rightSide-TextBox'
+            }
+          >
+            <h1>{data.name[0].toUpperCase() + data.name.slice(1, 9)}</h1>
+            <h4>{data.desc}</h4>
+            {Focus === data.name ? <p>{data.bio}</p> : null}
+            <button onClick={() => handleClick(data.name)}>
+              {Focus === data.name ? 'Volver' : 'Leer mas'}
+            </button>
+          </animated.div>
+        ) : (
+          <animated.div
+            style={Focus === 'caterina' ? props5 : null}
+            className={
+              side === 'left'
+                ? 'HomeStaff-container-leftSide-TextBox'
+                : 'HomeStaff-container-rightSide-TextBox'
+            }
+          >
+            <h1>{data.name[0].toUpperCase() + data.name.slice(1, 9)}</h1>
+            <h4>{data.desc}</h4>
+            {Focus === data.name ? <p>{data.bio}</p> : null}
+            <button onClick={() => handleClick(data.name)}>
+              {Focus === data.name ? 'Volver' : 'Leer mas'}
+            </button>
+          </animated.div>
+        )}
+      </animated.div>
+    );
+  }
+};
